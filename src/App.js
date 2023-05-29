4884ae2d9f72dfb73c6d10a6c6904d27c8a6be82
Sojitimothy11 <timothysoji1@gmail.com>
2023-05-29 07:41:23 +0100

    port

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
-import './App.css';
-import Navbar from './component/navbar';
-import Home from './component/home';
-import About from './component/about';
-import Portfolio from './component/portfolio';
-import Services from './component/services'
-   import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./component/navbar";
+import Home from "./component/home";
+import About from "./component/about";
+import Portfolio from "./component/portfolio";
+import Services from "./component/services";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
 
       <Routes>
-        <Route path="/home" exact element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/portfolio" element={<Portfolio/>}/>
-        <Route path="/services" element={<Services/>}/>  
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </div>
   );
